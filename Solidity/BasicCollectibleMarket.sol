889b89cc9bdb23396e7628a9851105e785144c41
thestriver <wannabeamumin@gmail.com>
2023-01-08 16:12:17 -0700

    chai hardhat test

--- Solidity/BasicCollectibleMarket.sol
+++ Solidity/BasicCollectibleMarket.sol
@@ -1,23 +1,37 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.4;
 
-contract Market {
-    // create the Collectible struct
-    struct Collectible {
-        address owner;
-        bool forSale;
-        uint price;
-    }
+contract Collectible {
+	address owner;
+	uint price;
 
-    // map a uint ID to a Collectible struct
-    mapping(uint => Collectible) idToCollectible;
+	event Deployed(address indexed);
+	constructor() {
+		owner = msg.sender;
+		emit Deployed(msg.sender);
+	}
 
-    function purchase(uint _id) external payable {
-        // find the collectible by the id passed in
-        Collectible storage collectible = idToCollectible[_id];
-        // purchase the collectible
-        require(msg.value >= collectible.price);
-        collectible.owner = msg.sender;
-        collectible.forSale = false;
-    }
+	event Transfer(address indexed, address indexed);
+	function transfer(address recipient) external {
+		require(msg.sender == owner);
+		owner = recipient;
+		emit Transfer(msg.sender, recipient);
+	}
+
+	event ForSale(uint, uint);
+	function markPrice(uint _price) external {
+		require(msg.sender == owner);
+		price = _price;
+		emit ForSale(price, block.timestamp);
+	}
+
+	event Purchase(uint, address indexed);
+	function purchase() payable external {
+		require(msg.value == price && price > 0);
+		price = 0;
+		(bool success, ) = owner.call{ value: msg.value }("");
+		require(success);
+		owner = msg.sender;
+		emit Purchase(msg.value, msg.sender);
+	} 
 }
