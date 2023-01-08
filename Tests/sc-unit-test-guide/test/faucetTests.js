e62b31708602f1291d088bddedf2899d076b7f32
thestriver <wannabeamumin@gmail.com>
2023-01-07 18:22:11 -0700

    chai hardhat test

--- Tests/sc-unit-test-guide/test/faucetTests.js
+++ Tests/sc-unit-test-guide/test/faucetTests.js
@@ -33,4 +33,46 @@ describe('Faucet', function () {
     await expect(faucet.connect(signer2).withdrawAll()).to.be.reverted
   });
 
+  it('should check that only owner can call destroyFaucet() address', async function () {
+    const { faucet, signer2 } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.connect(signer2).destroyFaucet()).to.be.reverted
+  });
+
+  it('should call destroyFaucet() successfully when owner calls it', async function () {
+    const { faucet } = await loadFixture(deployContractAndSetVariables);
+    let provider = ethers.getDefaultProvider();
+    await faucet.destroyFaucet();
+
+    expect(await provider.getCode(faucet.address)).to.equal("0x");
+  });
+
+  // it("should return funds to the owner successfully when selfdestruct is called", async function () {
+  //   const { faucet, owner } = await loadFixture(deployContractAndSetVariables);
+  //   let provider = ethers.getDefaultProvider();
+  //   const balanceBefore = await faucet.provider.getBalance(owner.address); //original balance
+  //   const balanceContractBefore = await faucet.provider.getBalance(
+  //     faucet.address
+  //   );
+
+  //   const tx = await faucet.destroyFaucet();
+  //   const txReceipt = await tx.wait(1);
+  //   const { gasUsed, effectiveGasPrice } = txReceipt;
+  //   const gasCost = gasUsed.mul(effectiveGasPrice);
+
+  //   const balanceContractAfter = await faucet.provider.getBalance(
+  //     faucet.address
+  //   );
+
+  //   const balanceAfter = await faucet.provider.getBalance(owner.address);
+
+  //   expect(balanceAfter.toString() > balanceBefore.toString()).to.be.true;
+
+  //   expect(balanceContractAfter.toString()).to.equal("0");
+
+  //   expect(balanceContractBefore.add(balanceBefore).toString()).to.equal(
+  //     balanceAfter.add(gasCost).toString()
+  //   );
+  // });
+
+
 });
