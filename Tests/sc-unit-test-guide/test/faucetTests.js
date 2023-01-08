1c7a6ef668ead73ceaec73ecac1d3a487d53d1ee
thestriver <wannabeamumin@gmail.com>
2023-01-07 18:16:04 -0700

    chai hardhat test

--- Tests/sc-unit-test-guide/test/faucetTests.js
+++ Tests/sc-unit-test-guide/test/faucetTests.js
@@ -9,10 +9,12 @@ describe('Faucet', function () {
     const Faucet = await ethers.getContractFactory('Faucet');
     const faucet = await Faucet.deploy();
 
-    const [owner] = await ethers.getSigners();
+    const [owner, signer2] = await ethers.getSigners();
+
+    let withdrawAmount = ethers.utils.parseUnits('1', 'ether');
 
     console.log('Signer 1 address: ', owner.address);
-    return { faucet, owner };
+    return { faucet, owner, withdrawAmount, signer2 };
   }
 
   it('should deploy and set the owner correctly', async function () {
@@ -20,4 +22,15 @@ describe('Faucet', function () {
 
     expect(await faucet.owner()).to.equal(owner.address);
   });
+
+  it('should send only 0.1 eth to users', async function () {
+    const { faucet, withdrawAmount } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.withdraw(withdrawAmount)).to.be.reverted;
+  });
+
+  it('should only withdraw all to owner address', async function () {
+    const { faucet, signer2 } = await loadFixture(deployContractAndSetVariables);
+    await expect(faucet.connect(signer2).withdrawAll()).to.be.reverted
+  });
+
 });
