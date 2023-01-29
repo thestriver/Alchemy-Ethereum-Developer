32d0efd99b7825dae8774cbb86f0f3c520172ef9
thestriver <wannabeamumin@gmail.com>
2023-01-29 15:59:50 -0700

    verify proxy and implementation

--- Solidity/my-upgradeable-contract/scripts/deployProxy.js
+++ Solidity/my-upgradeable-contract/scripts/deployProxy.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require('hardhat');
 
 async function main() {
   const VendingMachineV1 = await ethers.getContractFactory('VendingMachineV1');
-  const proxy = await upgrades.deployProxy(VendingMachineV1, [100]);
+  const proxy = await upgrades.deployProxy(VendingMachineV1, [100]); //deployproxy https://docs.openzeppelin.com/upgrades-plugins/1.x/
   await proxy.deployed();
 
   const implementationAddress = await upgrades.erc1967.getImplementationAddress(
