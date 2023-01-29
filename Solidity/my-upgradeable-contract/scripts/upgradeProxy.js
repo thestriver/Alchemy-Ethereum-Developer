7f44efea372b8e1386aa01832e0bc0c953697023
thestriver <wannabeamumin@gmail.com>
2023-01-29 16:18:02 -0700

    verify proxy and implementation

--- Solidity/my-upgradeable-contract/scripts/upgradeProxy.js
+++ Solidity/my-upgradeable-contract/scripts/upgradeProxy.js
@@ -1,6 +1,6 @@
 const { ethers, upgrades } = require('hardhat');
 
-const proxyAddress = '';
+const proxyAddress = '0x7aa1691951aBA75d4c2e4274b3C30580d7f41840';
 
 async function main() {
   const VendingMachineV2 = await ethers.getContractFactory('VendingMachineV2');
@@ -9,8 +9,8 @@ async function main() {
   const implementationAddress = await upgrades.erc1967.getImplementationAddress(
     proxyAddress
   );
-
-  console.log("The current contract owner is: " + upgraded.owner());
+  
+  console.log("The current contract owner is: " + await upgraded.owner() );
   console.log('Implementation contract address: ' + implementationAddress);
 }
 
